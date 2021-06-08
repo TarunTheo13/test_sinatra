063862445518518d8c10470e2f998f1cec1cb39a
Cynthia Fu <yinnycfu@gmail.com>
2021-06-08 15:19:25 +0100

    sinatra reloader reinstated, gemfile created

--- app.rb
+++ app.rb
@@ -1,10 +1,8 @@
 require 'sinatra'
-#require "sinatra/reloader" if development?
+require "sinatra/reloader" if development?
 
 get '/' do
-  "<div style='border: 3px dashed red'>
-    <img src='http://bit.ly/1eze8aE'>
-  </div>"
+  
 end
 
 get '/secret' do
