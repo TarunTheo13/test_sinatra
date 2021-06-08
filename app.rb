92de64fcb1cd0c72e15cebe8bd8a8a63f5515ea1
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-06-08 15:46:30 +0100

    adding params to webpage

--- app.rb
+++ app.rb
@@ -1,11 +1,16 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
 
-get '/' do
+get '/random-cat' do
   @cat_name = ['tarun', 'cynthia', 'buxton'].sample
   erb(:index)
 end
 
 get '/secret' do
   'Kṛṣṇas tu bhagavān svayam'
+end
+
+get '/named-cat' do
+  p @cat_name = params[:name]
+  erb(:index)
 end
