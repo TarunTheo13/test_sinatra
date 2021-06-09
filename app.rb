e62c9327ef35ebb02d25f120ce2b22fa43f05c58
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-06-09 14:53:47 +0100

    using post method

--- app.rb
+++ app.rb
@@ -10,7 +10,12 @@ get '/secret' do
   'Kṛṣṇas tu bhagavān svayam'
 end
 
-get '/named-cat' do
-  p @cat_name = params[:name]
+post '/named-cat' do
+  p params
+  @cat_name = params[:name]
   erb(:index)
+end
+
+get '/form' do
+  erb(:form)
 end
